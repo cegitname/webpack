0411ff89e86fd138912f1b256822513ebe8cc69f
wangce <80572451@qq.com>
2020-10-11 22:45:36 +0800

    css-loader postcssloader autoprefixer

--- demo_1/src/index.js
+++ demo_1/src/index.js
@@ -1,8 +1,13 @@
-import Header from './header'
-import Sider from './sidebar'
-import avatar from './assets/1.jpg'
+import Header from "./header";
+import Sider from "./sidebar";
+import avatar from "./assets/1.jpg";
+import "./index.less";
 
-console.log(avatar)
-new Header()
-new Sider()
-
+var img = new Image();
+img.src = avatar;
+img.classList.add("avatar");
+var root = document.getElementById("root");
+console.log(root)
+root.append(img);
+new Header();
+new Sider();
--- demo_1/webpack.config.js
+++ demo_1/webpack.config.js
@@ -1,18 +1,29 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  mode:"none",
+  mode: "none",
   entry: "./src/index.js",
-  module:{
-    rules:[{
-      test:/\.jpg$/,
-      use:{
-        loader:'file-loader'
-      }
-    }]
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name]_[hash]",
+            outputPath: "images/",
+            limit: 10240,
+          },
+        },
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader','css-loader','postcss-loader','less-loader'],
+      },
+    ],
   },
   output: {
     filename: "bsundle.js",
-    path: path.resolve(__dirname, "bundle")
-  }
-}
+    path: path.resolve(__dirname, "bundle"),
+  },
+};
