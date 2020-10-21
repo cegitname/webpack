0b4b424ce5e589aa01c5189d40010386821ba483
wangce <wangce_email@163.com>
2020-10-21 13:40:08 +0800

    loader,plugins,entry,output

--- demo_1/postcss.config.js
+++ demo_1/postcss.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   plugins:[
-    require('autoprefixer')
+    require('autoprefixer') // 自动添加webkit 配合postcss使用
   ]
 };
--- demo_1/webpack.config.js
+++ demo_1/webpack.config.js
@@ -1,8 +1,29 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
   mode: "none",
-  entry: "./src/index.js",
+  /*
+    entry 入口文件 
+    ***********************
+    默认值：默认打包生成的文件为main.js
+    entry:{
+      main:"./src/index.js"
+    }
+    ***********************
+    entry:'./src/index.js'
+    ***********************
+    打包生成2个文件 第一次为main 第二次为sub
+    entry:{
+      main:'./src/index.js',
+      sub:'./src/index.js'
+    },
+  */
+  entry:{
+    main:'./src/index.js',
+    sub:'./src/index.js'
+  },
   module: {
     rules: [
       {
@@ -17,13 +38,27 @@ module.exports = {
         },
       },
       {
-        test: /\.less$/,
-        use: ['style-loader','css-loader','postcss-loader','less-loader'],
+        test: /\.less$/, //遇到 less结尾的文件 执行use里的loader
+        use: ['style-loader','css-loader','postcss-loader','less-loader'], //执行顺序 从右到左
       },
     ],
   },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template:'src/index.html'
+    }),
+    // 打包之前 删除bundle目录下的所有内容
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns:['bundle']
+    })
+  ],
   output: {
-    filename: "bsundle.js",
+    // publicPath 打包生成的文件前添加cdn 
+    publicPath:'http://cnd.com.cn',
+    // filename: "[name].js", 打包生成的文件名为bundle.js，如果未配置filename ,默认为main.js
+    // [name] 指 entry配置的key值  main 和 sub
+    filename: "[name].js", 
+    // 打包到名为bundle的文件夹下
     path: path.resolve(__dirname, "bundle"),
   },
 };
