55d67d32986c17722eab97f446d16fa8b2ca5299
wangce <wangce_email@163.com>
2020-10-22 17:25:26 +0800

    hot module replacement plugin

--- sourceMap/src/index.js
+++ sourceMap/src/index.js
@@ -1,1 +1,9 @@
-console.lsog('hello world')
+import './style.css'
+var btn = document.createElement('button')
+btn.innerHTML= '点击'
+document.body.appendChild(btn)
+btn.onclick = function() {
+  var div = document.createElement('div')
+  div.innerHTML = 'item'
+  document.body.appendChild(div)
+}
--- sourceMap/webpack.config.js
+++ sourceMap/webpack.config.js
@@ -1,27 +1,33 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   mode: "development",
-  // srouce-map 可查看源文件 
-  // inline 把map文件打包到bundle.js中 提错源文件
-  // cheap  如果没有cheap会精确到那一行 哪个字符  cheap精确到行  打包性能提升
-  // moudle  除了自己的代码 同样检查第三方模块
-  // eval 打包速度最快 同意提示源代码 打包文件没有mapUrl  用eval()生成 提示内容不全面
-  // 推荐 cheap-module-eavl-source-map 提示全 打包快  dev环境
-  // 线上代码  cheap-moudle-source-map 
   devtool:'cheap-module-eavl-source-map ',
   entry:'./src/index.js',
+  devServer:{
+    // contentBase 服务器要在哪个文件夹下启动
+    contentBase:'./dist',
+    // 自动打开浏览器
+    open:true,
+    // 浏览器不自动更新
+    hot:true,
+    hotOnly:true
+  },
   module: {
+    rules:[{
+      test:/\.css$/,
+      use:[
+        'style-loader',
+        'css-loader',
+        'postcss-loader'
+      ]
+    }]
   },
   plugins:[
-    new HtmlWebpackPlugin({
-      template:'src/index.html'
-    }),
-    new CleanWebpackPlugin({
-      cleanAfterEveryBuildPatterns:['bundle']
-    })
+    new HtmlWebpackPlugin({  template:'src/index.html' }),
+    new webpack.HotModuleReplacementPlugin()
   ],
   output: {
     filename: "bunlde.js", 
