e7ddd74e59ffa901c48c7aa938ef9b291dfd1480
wangce <wangce_email@163.com>
2020-10-22 17:59:50 +0800

    hot module replacement

--- sourceMap/src/index.js
+++ sourceMap/src/index.js
@@ -1,9 +1,20 @@
-import './style.css'
-var btn = document.createElement('button')
-btn.innerHTML= '点击'
-document.body.appendChild(btn)
-btn.onclick = function() {
-  var div = document.createElement('div')
-  div.innerHTML = 'item'
-  document.body.appendChild(div)
+// import './style.css'
+// var btn = document.createElement('button')
+// btn.innerHTML= '点击'
+// document.body.appendChild(btn)
+// btn.onclick = function() {
+//   var div = document.createElement('div')
+//   div.innerHTML = 'item'
+//   document.body.appendChild(div)
+// }
+
+import counter from './content'
+import number from './number'
+counter()
+number()
+if(module.hot){
+  module.hot.accept('./number',()=>{
+    document.body.removeChild(document.getElementById('number'))
+    number()
+  })
 }
