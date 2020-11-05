0ad77bdec7534b0008a168d21bfecd598b6ec5cb
wangce <wangce_email@163.com>
2020-11-05 18:17:56 +0800

    babel 解析es66 按需引入 useBuiltins

---
+++ babelConfig/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  mode: "development",
+  devtool:'cheap-module-eavl-source-map ',
+  entry:'./src/index.js',
+  devServer:{
+    // contentBase 服务器要在哪个文件夹下启动
+    contentBase:'./dist',
+    // 自动打开浏览器
+    open:true,
+    // 浏览器不自动更新
+    hot:true,
+    hotOnly:true
+  },
+  module: {
+    rules:[
+      {
+      test:/\.css$/,
+      use:[
+        'style-loader',
+        'css-loader',
+        'postcss-loader'
+      ]
+     },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [['@babel/preset-env',{
+              useBuiltIns:'usage'
+            }]]  
+          }
+        }
+      }
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({  template:'src/index.html' }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: "bunlde.js", 
+    path: path.resolve(__dirname, "dist"),
+  },
+};
